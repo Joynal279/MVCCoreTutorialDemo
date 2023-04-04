6541e21561785ded03bfbf1302198468163c31a0
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-04-05 03:46:16 +0600

    added AddPerson method

--- MVCCoreTutorialDemo/Controllers/PersonController.cs
+++ MVCCoreTutorialDemo/Controllers/PersonController.cs
@@ -10,7 +10,20 @@ namespace MVCCoreTutorialDemo.Controllers
     {
         public IActionResult Index()
         {
+            //ViewBag & ViewData can send data only from controller to view
             ViewBag.greeting = "hello Joynal";
+            ViewData["greeting2"] = "I am using ViewData";
+
+            //TempData can send data from one controller method to another controller method
+            TempData["greeting3"] = "It's TempData message";
+
+
+
+            return View();
+        }
+
+        public IActionResult AddPerson()
+        {
             return View();
         }
     }
